5dec3ad80d2bfb77372e0aa9351044b48e7d67b3
yrdsm666 <47294670@qq.com>
2023-05-08 22:13:16 +0800

    add rich log information for provable broadcast

--- consensus/sDumbo/acs.go
+++ consensus/sDumbo/acs.go
@@ -3,12 +3,12 @@ package sDumbo
 import (
 	//"bytes"
 	"context"
-	//"encoding/hex"
+	// "encoding/hex"
 	"encoding/json"
 	//"errors"
 	//"github.com/golang/protobuf/proto"
 	"github.com/niclabs/tcrsa"
-	//"github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"
 	//"github.com/syndtr/goleveldb/leveldb"
 	go_hotstuff "github.com/wjbbig/go-hotstuff"
 	"github.com/wjbbig/go-hotstuff/config"
@@ -16,7 +16,7 @@ import (
 	"github.com/wjbbig/go-hotstuff/logging"
 	pb "github.com/wjbbig/go-hotstuff/proto"
 	//"os"
-	//"strconv"
+	"strconv"
 	//"sync"
 	"time"
 	"fmt"
@@ -28,12 +28,17 @@ var logger = logging.GetLogger()
 type CommonSubsetImpl struct {
 	consensus.AsynchronousImpl
 	
-	Sid              int
-	proposalHash     []byte         
-	cancel           context.CancelFunc
+	Sid                int
+	proposalHash       []byte         
+	cancel             context.CancelFunc
+	restart            chan bool
+	taskEndFlag        bool
+	msgEndFlag         bool
 
-	proBroadcast     ProvableBroadcast
-	mvbaInputVectors []MvbaInputVector
+	proBroadcast       ProvableBroadcast
+	// mvbaInputVectors map[int][]MvbaInputVector
+	mvbaInputVectors   []MvbaInputVector
+	futureVectorsCache []MvbaInputVector
 }
 
 type MvbaInputVector struct{
@@ -41,7 +46,6 @@ type MvbaInputVector struct{
 	sid           int
 	proposalHash  []byte
 	Signature     tcrsa.Signature
-
 }
 
 // sid: session id
@@ -54,7 +58,9 @@ func NewCommonSubset(id int) *CommonSubsetImpl {
 	}
 
 	msgEntrance := make(chan *pb.Msg)
+	restart := make(chan bool)
 	acs.MsgEntrance = msgEntrance
+	acs.restart =restart
 	acs.ID = uint32(id)
 	logger.WithField("replicaID", id).Debug("[ACS] Init command cache.")
 
@@ -72,14 +78,28 @@ func NewCommonSubset(id int) *CommonSubsetImpl {
 
 	acs.proBroadcast = NewProvableBroadcast(acs)
 
-	go acs.startNewInstance()
+	acs.futureVectorsCache = make([]MvbaInputVector, 0)
+
+	go acs.start(ctx)
 	go acs.receiveMsg(ctx)
+
+	acs.restart <- true
 	return acs
 }
 
 func (acs *CommonSubsetImpl) startNewInstance() {
-	acs.mvbaInputVectors = make([]MvbaInputVector, 0)
 	acs.Sid = acs.Sid + 1
+	acs.mvbaInputVectors = make([]MvbaInputVector, 0)
+
+	vectors := acs.futureVectorsCache[:]
+	for i, v := range vectors {
+		if v.sid == acs.Sid{
+			acs.mvbaInputVectors = append(acs.mvbaInputVectors, v)
+			acs.futureVectorsCache = append(acs.futureVectorsCache[:i], acs.futureVectorsCache[i+1:]...)
+		}
+	}
+
+	logger.Info("[replica_"+strconv.Itoa(int(acs.ID))+"] [sid_"+strconv.Itoa(int(acs.Sid))+"] startNewInstance")
 	var txs []string
 	for {
 		BatchSize := 2
@@ -97,6 +117,19 @@ func (acs *CommonSubsetImpl) startNewInstance() {
 	//acs.proBroadcast = *proBroadcast
 }
 
+func (acs *CommonSubsetImpl) start(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <- acs.restart:
+			acs.taskEndFlag = false
+			acs.msgEndFlag = false
+			go acs.startNewInstance()
+		}
+	}
+}
+
 func (acs *CommonSubsetImpl) receiveMsg(ctx context.Context) {
 	for {
 		select {
@@ -112,7 +145,7 @@ func (acs *CommonSubsetImpl) handleMsg(msg *pb.Msg) {
 	switch msg.Payload.(type) {
 	case *pb.Msg_Request:
 		request := msg.GetRequest()
-		//logger.WithField("content", request.String()).Debug("[ACS] Get request msg.")
+		// logger.WithField("content", request.String()).Debug("[ACS] Get request msg.")
 		// put the cmd into the cmdset
 		acs.TxnSet.Add(request.Cmd)
 		// send the request to the leader, if the replica is not the leader
@@ -122,6 +155,18 @@ func (acs *CommonSubsetImpl) handleMsg(msg *pb.Msg) {
 		senderId := int(pbFinal.Id)
 		senderSid := int(pbFinal.Sid)
 		senderProposalHash := pbFinal.Proposal
+		// Ignore messages from old sid
+		if senderSid < acs.Sid{
+			logger.WithFields(logrus.Fields{
+				"senderId":  senderId,
+				"senderSid":  senderSid,
+			}).Warn("[replica_"+strconv.Itoa(int(acs.ID))+"] [sid_"+strconv.Itoa(acs.Sid)+"] [PB] Get mismatch Pbfinal msg")
+			break
+		}
+		logger.WithFields(logrus.Fields{
+			"senderId":  senderId,
+			"senderSid":  senderSid,
+		}).Info("[replica_"+strconv.Itoa(int(acs.ID))+"] [sid_"+strconv.Itoa(int(acs.Sid))+"] [PB] Get PbFinal msg")
 		signature := &tcrsa.Signature{}
 		err := json.Unmarshal(pbFinal.Signature, signature)
 		if err != nil {
@@ -130,19 +175,19 @@ func (acs *CommonSubsetImpl) handleMsg(msg *pb.Msg) {
 		marshalData := getMsgdata(senderId, senderSid, senderProposalHash)
 		flag, err := go_hotstuff.TVerify(acs.Config.PublicKey, *signature, marshalData)
 		if ( err != nil || flag==false ) {
-			logger.WithField("error", err.Error()).Error("verfiy signature failed.")
+			logger.WithField("error", err.Error()).Error("[replica_"+strconv.Itoa(int(acs.ID))+"] [sid_"+strconv.Itoa(int(acs.Sid))+"] verfiy signature failed.")
 		}
-		// dataHash, err := go_hotstuff.CreateDocumentHash(marshalData, acs.Config.PublicKey)
-		// if err != nil {
-		// 	logger.WithField("error", err.Error()).Error("Create Document Hash failed.")
-		// }
 		wVector := MvbaInputVector{
 			id:            senderId,
 			sid:           senderSid,
 			proposalHash:  senderProposalHash,
 			Signature:     *signature,
 		}
-		acs.mvbaInputVectors = append(acs.mvbaInputVectors, wVector)
+		if senderSid > acs.Sid {
+			acs.futureVectorsCache = append(acs.mvbaInputVectors, wVector)
+		} else {
+			acs.mvbaInputVectors = append(acs.mvbaInputVectors, wVector)
+		}
 		if len(acs.mvbaInputVectors) == 2*acs.Config.F+1 {
 			fmt.Println("")
 			fmt.Println("---------------- [ACS] -----------------")
@@ -154,7 +199,16 @@ func (acs *CommonSubsetImpl) handleMsg(msg *pb.Msg) {
 			}
 			fmt.Println("[ACS] GOOD WORK!.")
 			fmt.Println("---------------- [ACS] -----------------")
+			// 需要保证旧实例结束再启动新实例
+			// 不能同时执行新旧实例，代码不允许
+			// 达到新实例的启动条件时，仍然要执行旧实例，不然其他节点可能无法在上一个实例中结束
+			// 也就是说，当节点广播完上一个实例的的pbfinal消息后，才可启动新实例
 			//go acs.startNewInstance()
+			// start a new instance if the conditions are met
+			acs.msgEndFlag = true
+			if acs.taskEndFlag == true {
+				acs.restart <- true
+			}
 		}
 		break
 	case *pb.Msg_PbValue:
--- consensus/sDumbo/provablebroadcast.go
+++ consensus/sDumbo/provablebroadcast.go
@@ -16,7 +16,7 @@ import (
 	//"github.com/wjbbig/go-hotstuff/logging"
 	pb "github.com/wjbbig/go-hotstuff/proto"
 	//"os"
-	//"strconv"
+	"strconv"
 	//"sync"
 	// "fmt"
 )
@@ -49,7 +49,8 @@ func NewProvableBroadcast(acs *CommonSubsetImpl) *ProvableBroadcastImpl {
 }
 
 func (prb *ProvableBroadcastImpl) startProvableBroadcast() {
-	logger.Debugf("[PROVABLE BROADCAST] Start Provable Broadcast")
+	logger.Info("[replica_"+strconv.Itoa(int(prb.acs.ID))+"] [sid_"+strconv.Itoa(prb.acs.Sid)+"] [PB] Start Provable Broadcast")
+
 	prb.EchoVote = make([]*tcrsa.SigShare, 0)
 
 	id := int(prb.acs.ID)
@@ -57,10 +58,7 @@ func (prb *ProvableBroadcastImpl) startProvableBroadcast() {
 	pbValueMsg := prb.acs.Msg(pb.MsgType_PBVALUE, id, prb.acs.Sid, prb.acs.proposalHash, nil)
 
 	// create msg hash
-	//fmt.Println("-------GOOD----------")
 	data := getMsgdata(id, prb.acs.Sid, prb.acs.proposalHash)
-	//fmt.Println(data)
-	//fmt.Printf("variable data is of type %T \n", data)
 	prb.DocumentHash, _ = go_hotstuff.CreateDocumentHash(data, prb.acs.Config.PublicKey)
 
 	// broadcast msg
@@ -68,50 +66,29 @@ func (prb *ProvableBroadcastImpl) startProvableBroadcast() {
 	if err != nil {
 		logger.WithField("error", err.Error()).Error("Broadcast failed.")
 	}
-
-	//return prb
 }
 
 func (prb *ProvableBroadcastImpl) handleProvableBroadcastMsg(msg *pb.Msg) {
 	switch msg.Payload.(type) {
 	case *pb.Msg_PbValue:
 		pbValueMsg := msg.GetPbValue()
-		logger.WithField("content", "").Debug("[ACS] Get PbValue msg.")
-		var senderId int = int(pbValueMsg.Id)
-		var senderSid int = int(pbValueMsg.Sid)
-		senderProposalHash := pbValueMsg.Proposal
-		// fmt.Println(senderId)
-		// fmt.Println(senderSid)
-		// fmt.Println(senderProposalHash)
-		// fmt.Println("----------------1---------------")
-		// fmt.Printf("variable prb is of type %T \n", prb)
-		// fmt.Println(prb.acs.Config)
-		marshalData := getMsgdata(senderId, senderSid, senderProposalHash)
-		// fmt.Println("------------- --2---------------")
-		// fmt.Println(prb.acs.Config.PublicKey)
-		// fmt.Println("----------------3---------------")
-		// fmt.Printf("variable marshalData is of type %T \n", marshalData)
+		senderId := int(pbValueMsg.Id)
+		senderSid := int(pbValueMsg.Sid)
+		logger.WithFields(logrus.Fields{
+			"senderId":  senderId,
+			"senderSid":  senderSid,
+		}).Info("[replica_"+strconv.Itoa(int(prb.acs.ID))+"] [sid_"+strconv.Itoa(prb.acs.Sid)+"] [PB] Get PbValue msg")
 
-		// defer func() {
-		// 	if r := recover(); r != nil {
-		// 		fmt.Println("Some error happened!", r)
-		// 		//ret = -1
-		// 	}
-		// }()
+		senderProposalHash := pbValueMsg.Proposal
+		marshalData := getMsgdata(senderId, senderSid, senderProposalHash)
 		documentHash, _ := go_hotstuff.CreateDocumentHash(marshalData, prb.acs.Config.PublicKey)
-		// fmt.Println("----------------4---------------")
-		// fmt.Printf("hash good\n %v\n", documentHash)
-		// logger.WithField("content", "hahahha").Debug("HELLO.")
-		// if err != nil {
-		// 	logger.WithField("error", err.Error()).Error("create Document Hash failed.")
-		// }
 		partSig, err := go_hotstuff.TSign(documentHash, prb.acs.Config.PrivateKey, prb.acs.Config.PublicKey)
 		if err != nil {
 			logger.WithField("error", err.Error()).Error("create the partial signature failed.")
 		}
 		partSigBytes, _ := json.Marshal(partSig)
 		pbEchoMsg := prb.acs.Msg(pb.MsgType_PBECHO, int(prb.acs.ID), senderSid, nil, partSigBytes)
-		// // reply msg to sender
+		// reply msg to sender
 		err = prb.acs.Unicast(prb.acs.GetNetworkInfo()[uint32(senderId)], pbEchoMsg)
 		if err != nil {
 			logger.WithField("error", err.Error()).Error("Unicast failed.")
@@ -122,7 +99,20 @@ func (prb *ProvableBroadcastImpl) handleProvableBroadcastMsg(msg *pb.Msg) {
 			break
 		}
 		pbEchoMsg := msg.GetPbEcho()
-		logger.WithField("content", "").Debug("[ACS] Get pbEcho msg.")
+		// Ignore messages from old sid
+		senderSid := int(pbEchoMsg.Sid)
+		if senderSid != prb.acs.Sid{
+			logger.WithFields(logrus.Fields{
+				"senderId":  int(pbEchoMsg.Id),
+				"senderSid":  senderSid,
+			}).Warn("[replica_"+strconv.Itoa(int(prb.acs.ID))+"] [sid_"+strconv.Itoa(prb.acs.Sid)+"] [PB] Get mismatch PbEcho msg")
+			break
+		}
+		logger.WithFields(logrus.Fields{
+			"senderId":  int(pbEchoMsg.Id),
+			"senderSid":  senderSid,
+		}).Info("[replica_"+strconv.Itoa(int(prb.acs.ID))+"] [sid_"+strconv.Itoa(prb.acs.Sid)+"] [PB] Get pbEcho msg")
+
 		partSig := &tcrsa.SigShare{}
 		err := json.Unmarshal(pbEchoMsg.SignShare, partSig)
 		if err != nil {
@@ -134,23 +124,39 @@ func (prb *ProvableBroadcastImpl) handleProvableBroadcastMsg(msg *pb.Msg) {
 			logger.WithFields(logrus.Fields{
 				"error":        err.Error(),
 				"documentHash": hex.EncodeToString(prb.DocumentHash),
-			}).Warn("[PROVABLE BROADCAST] pbEchoVote: signature not verified!")
+			}).Warn("[replica_"+strconv.Itoa(int(prb.acs.ID))+"] [sid_"+strconv.Itoa(prb.acs.Sid)+"] [PB] pbEchoVote: signature not verified!")
 			return
 		}
 
 		prb.EchoVote = append(prb.EchoVote, partSig)
 
 		if len(prb.EchoVote) == 2*prb.acs.Config.F+1 {
-			signature, _ := go_hotstuff.CreateFullSignature(prb.DocumentHash,  prb.EchoVote, prb.acs.Config.PublicKey)
+			signature, err := go_hotstuff.CreateFullSignature(prb.DocumentHash,  prb.EchoVote, prb.acs.Config.PublicKey)
+			if err != nil {
+				logger.WithFields(logrus.Fields{
+					"error":        err.Error(),
+					"documentHash": hex.EncodeToString(prb.DocumentHash),
+				}).Error("[replica_"+strconv.Itoa(int(prb.acs.ID))+"] [sid_"+strconv.Itoa(prb.acs.Sid)+"] [PB] pbEchoVote: create signature failed!")
+			}
 			prb.Signature = signature
+			logger.WithFields(logrus.Fields{
+				"signature":        len(signature),
+				"documentHash": len(prb.DocumentHash),
+				"echoVote": len(prb.EchoVote),
+			}).Warn("[replica_"+strconv.Itoa(int(prb.acs.ID))+"] [sid_"+strconv.Itoa(prb.acs.Sid)+"] [PB] pbEchoVote: create signature")
 			marshal, _ := json.Marshal(signature)
 			pbFinalMsg := prb.acs.Msg(pb.MsgType_PBFINAL, int(prb.acs.ID), prb.acs.Sid, prb.acs.proposalHash, marshal)
 			// broadcast msg
-			err := prb.acs.Broadcast(pbFinalMsg)
+			err = prb.acs.Broadcast(pbFinalMsg)
 			if err != nil {
 				logger.WithField("error", err.Error()).Error("Broadcast failed.")
 			}
-		
+			// start a new instance if the conditions are met
+			prb.acs.taskEndFlag = true
+			if prb.acs.msgEndFlag == true {
+				prb.acs.restart <- true
+			}
+			
 		}
 		break
 	default:
